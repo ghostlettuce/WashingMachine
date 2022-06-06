caaec66b95be2a83ad0eba92e150f0f8b05be7d4
ImoralDood <106877792+ImoralDood@users.noreply.github.com>
2022-06-06 16:40:43 +0100

    Create WashingMAchineFSM_Tb.vhd
    TB do fSM

---
+++ WashingMAchineFSM_Tb.vhd
@@ -0,0 +1,54 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+entity WashingMAchineFSM_Tb is
+end WashingMAchineFSM_Tb;
+
+architecture Stimulus of WashingMAchineFSM_Tb is
+		signal s_waterValve, s_waterpump, s_spin, s_rinse, s_newTime, s_timeEnable:std_logic;
+		
+		signal s_reset, s_startStop, s_clk, s_P1, s_P2, s_P3, s_porta, s_timeExp:std_logic
+
+begin 
+	 uut : entity work.WashingMAchineFSM(MealyArch);
+	port map(clk =>s_clk,
+								reset 		=> s_reset,
+								startStop 	=>s_startStop,
+								P1 			=> s_P1,
+								p2 			=> s_p2,
+								p3 			=> s_p3,
+								porta 		=> s_porta,
+								timeExp 		=>s_timeExp,
+								water_Valve => s_waterValve,
+								water_pump 	=> s_waterpump,
+								spin 			=> s_spin,
+								rinse 		=> s_rinse,
+								newTime 		=> s_newTime,
+								timeEnable	=> s_timeEnable
+											
+							
+);
+
+
+clk_proc : process
+		begin
+			s_clk <= '0'; wait for 10 ns;
+			s_clk <= '1'; wait for 10 ns;
+		end process;
+
+		
+stimulus_process : process
+		begin
+		
+		
+		
+		
+		end process;
+		
+		
+		
+		
+
+
+		
+
